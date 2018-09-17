2b7fa36ac0e2bca37113626fecb83e0911950827
kevin226j <kevin226j@gmail.com>
2018-09-17 16:02:25 -0700

    Dashboard view -> p1:  ++banner with nav

--- mean-app/src/app/app.module.ts
+++ mean-app/src/app/app.module.ts
@@ -2,13 +2,23 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+
+// views
+import {DashboardComponent} from './dashboard/dashboard.component';
+
+//components
+import {BannerComponent} from './components/banner/app.banner.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent,
+    BannerComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ mean-app/src/app/components/banner/app.banner.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-banner-component',
+  templateUrl: './app.banner.component.html',
+  styleUrls: []
+})
+export class BannerComponent implements OnInit {
+  constructor() { }
+  ngOnInit() {
+  }
+}
